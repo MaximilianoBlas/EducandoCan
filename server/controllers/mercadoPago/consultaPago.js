78b444e891d20ba3f2ee3400e9509cfa7cdbec8b
MaximilianoSilva <silvamaxiblas@gmail.com>
2024-09-17 23:33:33 -0300

    get payment

--- server/controllers/mercadoPago/consultaPago.js
+++ server/controllers/mercadoPago/consultaPago.js
@@ -1,14 +1,14 @@
 // const { models } = require('../../database/connection/database')
 const config = require('../../config')
-const {MercadoPagoConfig, Preference, Payment} = require('mercadopago')
-const MercadoPago = require('mercadopago')
+const {MercadoPagoConfig, Preference, Payment, MercadoPago} = require('mercadopago')
+// const MercadoPago = require('mercadopago')
 
 
  const consultaPago = async (req, res) => {
   const {name,amount} = req.query
 
  try {
-    const client = new MercadoPago({ accessToken: config.MP_PUBLIC_KEY });
+    const client = new MercadoPagoConfig({ accessToken: config.MP_PUBLIC_KEY });
     const payment = new Payment(client);
 
    const pago =  payment.get({
