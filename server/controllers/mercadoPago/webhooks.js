104e38b25295a4016330b16674165defa1687d0b
MaximilianoSilva <silvamaxiblas@gmail.com>
2024-09-19 20:41:50 -0300

    wh upd

--- server/controllers/mercadoPago/webhooks.js
+++ server/controllers/mercadoPago/webhooks.js
@@ -2,13 +2,13 @@ const { consultaPago } = require("./consultaPago")
 
 
  const webhooks = async (req, res) => {
-    const {data} = req.query
+    const {type} = req.query
   console.log(req.query)
-  console.log('data', data)
-  console.log('data.id', data.id)
+  console.log('type', type)
+  console.log('data', req.query['data.id'])
   console.log('estamos en el webhooks backend')
 
-  const pago = consultaPago(data.id)
+  const pago = consultaPago(data)
   console.log('pago en webhooks', pago)
 
  try {
