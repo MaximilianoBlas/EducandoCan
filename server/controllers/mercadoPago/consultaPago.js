ba12e04f928f7e29b6cfc8f318578fdd783d95fb
MaximilianoSilva <silvamaxiblas@gmail.com>
2024-09-20 19:39:24 -0300

    wh upd

--- server/controllers/mercadoPago/consultaPago.js
+++ server/controllers/mercadoPago/consultaPago.js
@@ -21,11 +21,12 @@ const {MercadoPagoConfig, Preference, Payment, MercadoPago} = require('mercadopa
         id: param,
 })
 
+
 console.log('pago',pago)
 console.log('pago additional info item',pago.additional_info)
 console.log('pago card cardholder',pago.card.cardholder)
-console.log('pago additional info item',pago.charges_details.accounts)
-console.log('pago additional info item',pago.charges_details.amounts)
+console.log('pago accounts',pago.charges_details[0].accounts)
+console.log('pago amounts',pago.charges_details[0].amounts)
 
         //  res.json(pago)
         return pago
