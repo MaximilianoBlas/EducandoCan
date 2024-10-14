1962fbb0494d451ccf89fbca8f39a37aadca3d24
MaximilianoSilva <silvamaxiblas@gmail.com>
2024-10-14 17:49:39 -0300

    style

--- src/app/clases/page.jsx
+++ src/app/clases/page.jsx
@@ -11,7 +11,9 @@ import {consultarApiMercadoPago, webhooks} from '../Redux/action/mercadoPago'
 import { useRouter } from 'next/navigation';
 import { upDateCalendar } from '../Redux/action/calendar';
 import { getCurrentDollar } from '../Redux/action/currentDollar';
-
+import Image from 'next/image'
+import presencial from '../../../public/presencial.webp'
+import online from '../../../public/online.webp'
 
 export default function Guardas() {
   const dispatch = useDispatch()
@@ -25,8 +27,17 @@ export default function Guardas() {
   const [inPersonView, setInPersonView] = useState(false)
   const [onlineView, setOnlineView] = useState(false)
   const [formView, setFormView] = useState(false)
-  const [typeClass, setTypeClass] = useState('')
   const [dateGuard, setDateGuard] = useState('')
+  const [errors,setErrors] = useState({
+      clientName:'',
+      clientLastname:'',
+      email:'',
+      phone:'',
+      date:'',
+      description:''
+  })
+  const [warningSign, setWarninSing] = useState(false)
+  const [disabledButton, setDisabledButton] = useState(false)
   const [form, setForm] = useState({
     startDate:'',
     endDate:'',
@@ -34,7 +45,7 @@ export default function Guardas() {
     clientLastname:'',
     email: '',
     phone: '',
-    description: '', 
+    description: '',
     amount:'',
     type:''
   })
@@ -43,24 +54,58 @@ export default function Guardas() {
 
   if(preference)router.push(preference)
 
-    
+
     useEffect(()=>{
   dispatch(upDateCalendar())
   dispatch(getCurrentDollar())
   },[])
 
   useEffect(()=>{
+
     let currentEvent = []
         calendar.forEach(e => {
-      let start = e.startDate
-      let end = e.endDate
+          let month = e.startDate.split('-')[1]
+          let newStart , newEnd
+          if(month >= 10 && month <= 12){
+            
+            newStart = e.startDate.split('-')
+            newStart[1] = `0${month}`
+            newStart.join('-')
+            newEnd = e.endDate.split('-')
+            newEnd[1] = `0${month}`
+            newEnd.join('-')
+          } else {
+            newStart = e.startDate
+            newEnd = e.startDate
+          }
+
  currentEvent.push({...e,
         title: e.name,
-         start: dayjs(start).toDate(),
-      end : dayjs(start).toDate(),})
+         start: dayjs(newStart).toDate(),
+      end : dayjs(newEnd).toDate(),})
     });
     setEvent(currentEvent)
     },[calendar])
+
+
+    const hourAfterClass = (hour, minute) => {
+      let startClass
+      let endClass
+      startClass = `${Number(hour)-2}:${minute}`
+      if(form.type === 'inPerson') {
+        endClass = `${Number(hour)+2}:${minute}`}
+      else{ if(minute > 29) {
+        endClass = `${Number(hour) + 2}:${Number(minute) - 30}`}
+         else {
+        endClass = `${Number(hour) + 1}:${Number(minute) + 30}`}}
+
+ 
+
+        if(startClass && startClass.length === 4)  startClass = 0 + startClass
+        if(endClass && endClass.length === 4)  endClass = 0 + endClass 
+        return {startClass, endClass}
+    }
+  
 
   const localizer = dayjsLocalizer(dayjs)
 
@@ -73,10 +118,12 @@ export default function Guardas() {
   }
 
   const createEvent = (e) => {
-    if(view === 'month') setView('day')
+    if(!optionView && !inPersonView && !onlineView && !formView){
+    if(view === 'month') {
+      setDate(e.start)
+      setView('day')}
       else{
-
-        setOptionView(!optionView)
+        setOptionView(true)
     let {start} = e
     let string = start.toString()
     let stringArray = string.split(' ')
@@ -85,16 +132,23 @@ export default function Guardas() {
       month:stringArray[1],
       number:stringArray[2],
       year:stringArray[3],
-      hour:stringArray[4],
+      hour:stringArray[4].split(':').shift(),
+      minute:stringArray[4].split(':').slice(1,2).join('')
     }
-    console.log(stringArray[4].split(':').shift())
-    console.log(stringArray[4] === '00:00:00')
-  if(stringArray[4] === '00:00:00') date = {...date, hour:''} 
-  else {date = {...date, hour:stringArray[4]}
+
+    
+        const after = hourAfterClass(date.hour, date.minute)
+    
+    
+    if(stringArray[4] === '00:00:00') date = {...date, hour:''}
+    else {date = {...date, hour:stringArray[4]}  
     setHour(stringArray[4].split(':').slice(0,2).join(':'))}
-  
-    console.log(date.hour)
-    setForm({...form, startDate: `${date.year}-${numberMonth[date.month]}-${date.number}-${date.hour}`, amount:currentDollar*50})
+
+    const startDate = `${date.year}-${numberMonth[date.month]}-${date.number}-${date.hour}`
+
+    validate(e,startDate,after.endClass, after.startClass)
+
+    setForm({...form, startDate: startDate, amount:currentDollar*50})
     setDateGuard(date)
     if(formView) {
       setForm({ startDate:'',
@@ -103,18 +157,18 @@ export default function Guardas() {
       clientLastname:'',
       email: '',
       phone: '',
-      description: '', 
+      description: '',
       amount:'',
       type:'' })}
-    
+
       }
-    
+    }
   }
 
 
 
 
-  
+
 
   const days = {
     Sun:'Domingo',
@@ -175,35 +229,48 @@ export default function Guardas() {
 
   const scheduleGuard = () => {
     dispatch(consultarApiMercadoPago(form))
-    
-    // dispatch(webhooks())
   }
 
+  
+
   const completedForm = (e) => {
-    if(form.startDate && e.type === 'time'){      
+  
+    if(form.startDate && e.type === 'time'){
       setHour(e.value)
       let startDate =  form.startDate.split('-')
       startDate[3] = e.value
       let newStart = startDate.join('-')
 
-      setForm({...form, startDate:newStart})
+      let newHour = e.value
+      let [hour,minute] = newHour.split(':')
+      
+      let after = hourAfterClass(hour, minute)
 
+       startDate =  form.startDate.split('-')
+      startDate[3] = after.endClass
 
-      // if(startDate[1]*1 === endDate[1]*1){
-      //   let amount = Number(endDate[2]) - Number(startDate[2])
-      //   setForm({...form,[e.name]:e.value, amount:amount*10000})
-      // } else{
-      //   let numberMonth = extensionMonth[dateGuard.month]
-      //   let amount = numberMonth - Number(startDate[2]) + Number(endDate[2])
-      //   setForm({...form,[e.name]:e.value, amount:amount*10000})
-      // }
-    }else setForm({...form,[e.name]:e.value})
+      let newEnd = startDate.join('-')
+
+      setErrors(validate(e, newStart, after.endClass, after.startClass))
+      setForm({...form, startDate:newStart, endDate:newEnd})
+
+    }else {
+      let startDate =  form.startDate.split('-')
+      let newHour = startDate[3]
+      let [hour,minute] = newHour.split(':')
+
+      let end = hourAfterClass(hour, minute)
+
+       startDate =  form.startDate.split('-')
+      startDate[3] = end
+      let newEnd = startDate.join('-')
+      
+      setErrors(validate({...form, [e.name]: e.value}))
+      setForm({...form,[e.name]:e.value,endDate:newEnd})}
   }
 
   const selectClass = (e) => {
-    console.log(e.target.value)
-    // setTypeClass('a ver')
-    setOnlineView(false)
+    setOptionView(false)
     if(e.target.value === 'online') {
       setForm({...form, type:'online'})
       setOnlineView(true)}
@@ -212,36 +279,117 @@ export default function Guardas() {
       setInPersonView(true)}
   }
 
-  console.log(view)
 
+
+  const validate = (input,newStart,endClass,startClass) =>{
+
+    if(newStart){
+      let dateOutOfTime = newStart.split('-').slice(0,3).join('-')
+ 
+      let date= ''
+       date = calendar.find(e  => {
+        let endTime = e.startDate.split('-').slice(0,3).join('-')
+        let timeOfDateChange = e.startDate.split('-').pop()
+        timeOfDateChange = timeOfDateChange.split(':').slice(0,2).join(':')
+
+        return dateOutOfTime === endTime ? timeOfDateChange >= startClass ? timeOfDateChange <= endClass ? true : false :false :false
+      } )
+
+      if(date){
+        setWarninSing(true)
+          setDisabledButton(true)
+      } else {
+        setWarninSing(false)
+        setDisabledButton(false)}
+      } 
+
+    let errors = {};
+    if (!input.clientName) {
+        errors.clientName = "El nombre es requerido";
+    } else if (/([0-9])/.test(input.clientName)) {
+        errors.clientName = "Nombre invalido, solo se permiten letras";
+    }
+    if (!input.clientLastname) {
+      errors.clientLastname = "El apellido es requerido";
+  } else if (/([0-9])/.test(input.clientLastname)) {
+      errors.clientLastname = "Apellido invalido, solo se permiten letras";
+  }
+  if (!input.email) {
+    errors.email = "El mail es requerido";
+} else if (!(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(input.email))) {
+    errors.email = "El formato del mail es incorrecto";
+}
+if (!input.phone) {
+  errors.phone = "El celular es requerido";
+} else if (!(/^\d{1,15}$/.test(input.phone))) {
+  errors.phone = "Solo se permiten números";
+}
+  if(Object.keys(errors).length === 0) setDisabledButton(false)
+    else setDisabledButton(true)
+    return errors;
+
+  }
+
+
+  const closeInfoView = () => {
+    setOnlineView(false)
+     setFormView(true)
+     setInPersonView(false)
+  }
 
 
   return (
     <div className={style.divContainer}>
-      {/* <button onClick={()=>onView()} style={{width:'10vw', height:'5vh'}}>view change</button> */}
    <Calendar selectable
-   onSelectSlot={(e) => {createEvent(e)}} 
-    localizer={localizer} events={event} view={view} date={date} 
-    onView={(e)=> onView(e)} 
+   onSelectSlot={(e) => {createEvent(e)}}
+    localizer={localizer} events={event} view={view} date={date}
+    onView={(e)=> onView(e)}
     onNavigate={(e)=> onNavigate(e)} />
 
     {
       optionView &&
       <div className={style.formContainer}>
-        <button value={'inPerson'} onClick={(e) =>{selectClass(e)}}>Clase presencial</button>
-        <button value={'online'} onClick={(e) =>{selectClass(e)}}>Clase online</button>
+        <div className={style.closeButtonContainer}>
+        <button className={style.closeButton} onClick={() => setOptionView(false)}>X</button>
+        </div>
+        <div className={style.presentialAndOnlineButtonContainer}>
+
+
+        <div className={style.presentialAndOnlineButton}>
+        <button className={style.button} value={'inPerson'} onClick={(e) =>{selectClass(e)}}>
+        <Image src={presencial} style={{
+          width: '83%',
+          height: 'auto',
+        }} alt='foto'></Image> 
+          </button>
+          <p>Clase presencial</p>
+        </div>
+      
+      <div className={style.presentialAndOnlineButton}>
+        <button className={style.button} value={'online'} onClick={(e) =>{selectClass(e)}}>
+        <Image src={online} style={{
+          width: '100%',
+          height: 'auto',
+        }} alt='foto'></Image></button>
+          <p> Clase online</p>
+      </div>
+          </div>
       </div>
     }
 
 {
       onlineView &&
       <div className={style.formContainer}>
+      <div className={style.closeButtonContainer}>
+        <button className={style.closeButton} onClick={() => setOnlineView(false)}>X</button>
+        </div>
+
         <div className={style.pAndButton}>
         <p className={style.p}>
         ¿Qué es Lorem Ipsum?
         Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
         </p>
-        <button  onClick={(e) =>{setOnlineView(false), setFormView(true)}}>Continuar</button>
+        <button className={style.button}  onClick={(e) =>{closeInfoView()}}>Continuar</button>
         </div>
       </div>
     }
@@ -249,51 +397,65 @@ export default function Guardas() {
 {
       inPersonView &&
       <div className={style.formContainer}>
+        <div className={style.closeButtonContainer}>
+        <button className={style.closeButton} onClick={() => setInPersonView(false)}>X</button>
+        </div>
         <div className={style.pAndButton}>
         <p className={style.p}>
         ¿Qué es Lorem Ipsum?
         Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.
         </p>
-     <button  onClick={(e) =>{setInPersonView(false), setFormView(true)}}>Continuar</button>
+     <button className={style.button}  onClick={(e) =>{closeInfoView()}}>Continuar</button>
         </div>
       </div>
     }
 
-
-
     {
       formView &&
-    <div className={style.formContainer}>
-      {/* <div className={style.inputContainer}> */}
-      <h3>Agenda de Clase</h3>
-      
+        <div className={style.formContainer}>
+      <div className={style.closeButtonContainer}>
+        <button className={style.closeButton} onClick={() => {setFormView(false), setWarninSing(false)}}>X</button>
+        </div>
+        <div className={style.containerWithoutCloseButton}>
+
+        
+      <h2>Agenda de Clase</h2>
+      <div className={style.inputTimeContainer}>
       <div className={style.inputContainer}>
       <div>
       <label htmlFor="start">{`${days[dateGuard.day]} ${dateGuard.number} ${months[dateGuard.month]} ${hour}`}</label>
       </div>
       <div>
       <label htmlFor="end"></label>
-      <input value={hour} onChange={(e) => {completedForm(e.target)}} type='time'  name='endDate' />
+      <input className={style.input} value={hour} onChange={(e) => {completedForm(e.target)}} type='time'  name='endDate' />
         </div>
+      </div>
+      {warningSign && <p className={style.warningSignP}>Horario reservado, 
+        cambiar horario por favor</p>}
+
       </div>
       <div className={style.inputContainer}>
       <div>
       <label htmlFor="name">Nombre</label>
-      <input  value={form.clientName} onChange={(e) => {completedForm(e.target)}} type="text" name='clientName' />
+      <input className={style.input}  value={form.clientName} onChange={(e) => {completedForm(e.target)}} type="text" name='clientName' />
+      {errors.clientName && <p className={style.warningSignP}>{errors.clientName}</p>}
       </div>
       <div>
       <label htmlFor="lastname">Apellido</label>
-      <input value={form.clientLastname} onChange={(e) => {completedForm(e.target)}} type="text" name='clientLastname' />
+      <input className={style.input} value={form.clientLastname} onChange={(e) => {completedForm(e.target)}} type="text" name='clientLastname' />
+      {errors.clientLastname && <p className={style.warningSignP}>{errors.clientLastname}</p>}
         </div>
       </div>
 <div className={style.inputContainer}>
         <div >
       <label htmlFor="email">Email</label>
-      <input value={form.email} onChange={(e) => {completedForm(e.target)}} type="text" name='email' />
+      <input className={style.input} value={form.email} onChange={(e) => {completedForm(e.target)}} type="text" name='email' />
+      {errors.email && <p className={style.warningSignP}>{errors.email}</p>}
         </div>
         <div>
       <label htmlFor="phone">Celular</label>
-      <input value={form.phone} onChange={(e) => {completedForm(e.target)}} type="text" name='phone' />
+      <input className={style.input} value={form.phone} onChange={(e) => {completedForm(e.target)}} type="text" name='phone' />
+      {errors.phone && <p className={style.warningSignP}>{errors.phone}</p>}
         </div>
 </div>
         <div className={style.TextareaContainer}>
@@ -301,10 +463,17 @@ export default function Guardas() {
       <textarea value={form.description} onChange={(e) => {completedForm(e.target)}}  minLength={10}  rows={5} type="text" name='description' />
         </div>
         {/* </div> */}
-        <h6>Costo de clase {`${currentDollar*50}`}</h6>
-        <button onClick={(e) =>{scheduleGuard(e)}}>Agendar</button>
+        <h4>Costo de clase {`${currentDollar*50}`}</h4>
+        <button className={style.button} disabled={disabledButton} onClick={(e) =>{scheduleGuard(e)}}>Agendar</button>
+        </div>
     </div>
     }
+    {/* {warningSign &&
+    <div className={style.warningSignContainer}>
+      El horario para la clase seleccionada esta ocupado. 
+      Por favor cambio el horario o el día. 
+    </div>
+    } */}
     </div>
   )
 }
