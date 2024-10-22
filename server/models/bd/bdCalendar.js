0c7d13b989d098400630366e2373a86a7fb0812a
MaximilianoSilva <silvamaxiblas@gmail.com>
2024-10-21 21:25:29 -0300

    admin view

--- server/models/bd/bdCalendar.js
+++ server/models/bd/bdCalendar.js
@@ -36,7 +36,7 @@ module.exports = (sequelize) => {
         allowNull: false,
       },
       phone: {
-        type: DataTypes.NUMBER,
+        type: DataTypes.STRING,
         allowNull: false,
       },
       description: {
