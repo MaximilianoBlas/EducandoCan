64aed1c25b99bbfbc09fc42fcf4edc488bd552bd
MaximilianoSilva <silvamaxiblas@gmail.com>
2024-10-23 20:51:28 -0300

    admin view

--- server/server.js
+++ server/server.js
@@ -23,23 +23,23 @@ app.use(express.urlencoded({extended:true}))
 // app.use(cors())
 
 
-// app.use((req, res, next) => {
-//     res.header(
-//         "Access-Control-Request-Methods",
-//         " POST"
-//     );
-//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     res.header(
-//         "Access-Control-Allow-Headers",
-//         "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     res.header(
-//         "Access-Control-Allow-Methods",
-//         "GET, POST, OPTIONS, PUT, DELETE"
-//     );
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header(
+        "Access-Control-Request-Methods",
+        " POST"
+    );
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE"
+    );
+    next();
+});
 
 // app.use((req, res, next) => {
 //     res.setHeader('Access-Control-Allow-Origin','*')
