73b508a1244514ea77db2f9155ded3b8340b9765
MaximilianoSilva <silvamaxiblas@gmail.com>
2024-10-23 20:55:31 -0300

    admin view

--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
 // }))
 
 app.use(cors({
-    origin: 'http://localhost:3000'|| 'https://educando-can.vercel.app/', // Aquí puedes especificar el dominio que permites
+    origin: 'http://localhost:3000', // Aquí puedes especificar el dominio que permites
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos que permites
     credentials: true, // Si necesitas enviar cookies o autenticación
 }));
